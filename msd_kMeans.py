c05b59ecfedd52cd4be129255000cfae67215c23
Peter Fan <cfa60@sfu.ca>
2022-12-05 23:17:45 -0800

    xp sprint

--- msd_kMeans.py
+++ msd_kMeans.py
@@ -41,7 +41,7 @@ def main(inputs, output):
         MinMaxScaler(inputCol='title_vec', outputCol='title_scaled')
     ])
 
-    stringTypes_cols = ['artist_id_vec_scaled', 'artist_name_scaled', 'title_scaled']
+    stringTypes_cols = ['artist_id_scaled', 'artist_name_scaled', 'title_scaled']
 
     ######################################################################################
 
@@ -64,7 +64,7 @@ def main(inputs, output):
     floatTypes = [
         'artist_familiarity', # use to filter out low familiarity artists?
         'artist_hotttnesss',
-        'artist_latitude', 'artist_longitude',
+        # 'artist_latitude', 'artist_longitude',    # dropped because too many N/A
         # 'song_hotttnesss', # dropped because too many N/A
         'danceability', 'energy',
         'end_of_fade_in', 'start_of_fade_out',
@@ -73,18 +73,73 @@ def main(inputs, output):
         'loudness', #TODO: shift +60dB (since -60dB is the zero in raw data)
         'tempo', 
         'mode', 'mode_confidence',
-        'time_signature', 'time_signature_confidence',
+        'time_signature', 'time_signature_confidence',  # beats per min
         'year', #TODO: shift to delta since min year
     ]
     """ Transformers for float/int type columns """
-    artist_familiarity_tf = MinMaxScaler(inputCol='artist_familiarity', outputCol='artist_familiarity_scaled')
-    artist_hotttnesss_tf = MinMaxScaler(inputCol='artist_hotttnesss', outputCol='artist_hotttnesss_scaled')
-    artist_latitude_tf = MinMaxScaler(inputCol='artist_latitude', outputCol='artist_latitude_scaled',
-        min=-90.0, max=90.0)
-    artist_longitude_tf = MinMaxScaler(inputCol='artist_longitude', outputCol='artist_longitude_scaled',
-        min=-180.0, max=180.0)
+    artist_familiarity_tf = Pipeline(stages=[
+        VectorAssembler(inputCols=['artist_familiarity'], outputCol='artist_familiarity_vec'),
+        MinMaxScaler(inputCol='artist_familiarity_vec', outputCol='artist_familiarity_scaled')
+    ])
+    artist_hotttnesss_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['artist_hotttnesss'], outputCol='artist_hotttnesss_vec'),
+        MinMaxScaler(inputCol='artist_hotttnesss_vec', outputCol='artist_hotttnesss_scaled')
+    ])
+    # artist_latitude_tf = MinMaxScaler(inputCol='artist_latitude', outputCol='artist_latitude_scaled')
+    # artist_longitude_tf = MinMaxScaler(inputCol='artist_longitude', outputCol='artist_longitude_scaled')
+    danceability_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['danceability'], outputCol='danceability_vec'),
+        MinMaxScaler(inputCol='danceability_vec', outputCol='danceability_scaled')
+    ])
+    energy_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['energy'], outputCol='energy_vec'),
+        MinMaxScaler(inputCol='energy_vec', outputCol='energy_scaled')
+    ])
+    end_of_fade_in_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['end_of_fade_in'], outputCol='end_of_fade_in_vec'),
+        MinMaxScaler(inputCol='end_of_fade_in_vec', outputCol='end_of_fade_in_scaled')
+    ])
+    start_of_fade_out_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['start_of_fade_out'], outputCol='start_of_fade_out_vec'),
+        MinMaxScaler(inputCol='start_of_fade_out_vec', outputCol='start_of_fade_out_scaled')
+    ])
+    key_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['key'], outputCol='key_vec'),
+        MinMaxScaler(inputCol='key_vec', outputCol='key_scaled')
+    ])
+    key_confidence_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['key_confidence'], outputCol='key_confidence_vec'),
+        MinMaxScaler(inputCol='key_confidence_vec', outputCol='key_confidence_scaled')
+    ])
+    duration_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['duration'], outputCol='duration_vec'),
+        MinMaxScaler(inputCol='duration_vec', outputCol='duration_scaled')
+    ])
+    loudness_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['loudness'], outputCol='loudness_vec'),
+        MinMaxScaler(inputCol='loudness_vec', outputCol='loudness_scaled')
+    ])
+    tempo_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['tempo'], outputCol='tempo_vec'),
+        MinMaxScaler(inputCol='tempo_vec', outputCol='tempo_scaled')
+    ])
+    mode_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['mode'], outputCol='mode_vec'),
+        MinMaxScaler(inputCol='mode_vec', outputCol='mode_scaled')
+    ])
+    time_signature_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['time_signature'], outputCol='time_signature_vec'),
+        MinMaxScaler(inputCol='time_signature_vec', outputCol='time_signature_scaled')
+    ])
+    year_tf = Pipeline(stages=[ 
+        VectorAssembler(inputCols=['year'], outputCol='year_vec'),
+        MinMaxScaler(inputCol='year_vec', outputCol='year_scaled')
+    ])
 
-    floatTypes = ['', '']
+    floatTypes_cols = ['artist_familiarity_scaled', 'artist_hotttnesss_scaled', 'danceability_scaled', 'energy_scaled',
+        'end_of_fade_in_scaled', 'start_of_fade_out_scaled', 'key_scaled', 'key_confidence_scaled', 'duration_scaled',
+        'loudness_scaled', 'tempo_scaled', 'mode_scaled', 'time_signature_scaled', 'year_scaled']
+
     ######################################################################################
     floatArrTypes = [
         # 'artist_terms_freq', 'artist_terms_weight', #TODO: VectorAssemble with 'artist_terms'
@@ -101,32 +156,9 @@ def main(inputs, output):
 
     # Select from the raw DF
     feature_cols = stringTypes + stringArrTypes + floatTypes + floatArrTypes + specialArrTypes
+
     df = df.select(feature_cols)
-    
-    # TODO: an UDF to pad 1D vectors with zeros to max length
-    # TODO: an UDF to pad 2D vectors with zeros to max length
 
-    # TODO: For numeric data field used, generate a distribution graph pre- post-transform
-
-    # Use Word2Vec to transform String type data into Vectors
-    # TODO: do this for all stringTypes
-
-
-
-
-    # TODO: Convert Float ArrayTypes to Vectors using array_to_vector
-    # df = df.withColumn(
-    #     'segments_start', array_to_vector(df['segments_start'])
-    # )
-
-    # TODO: Convert 2D Float ArrayTypes to Vectors
-    df = df.withColumn(
-        'segments_timbre_vec', 
-        functions.transform(        # map each element (https://spark.apache.org/docs/3.2.0/api/python/reference/api/pyspark.sql.functions.transform.html)
-            'segments_timbre',
-            lambda x : array_to_vector(x)
-        )
-    ).withColumn('segments_timbre_vec', array_to_vector('segments_timbre_vec'))
 
 
     # Drop rows with any empty fields
@@ -134,31 +166,44 @@ def main(inputs, output):
     rows = df.count() # should have less than 10k songs now
     print(f"Number of rows left: {rows}")
 
-
     """
     Build pipeline
     """
     # Transform all features into a Vector
     ignore = [] # columns to ignore (str)
-    vectors = ['artist_id_vec', 'filename_vec', 'energy', 'similar_artists_vec',
-        'danceability', 'segments_start']
+    scaled_features = stringTypes_cols + stringArrTypes_cols + floatTypes_cols
     final_assembler = VectorAssembler(
         # inputCols=[x for x in df.columns if x not in ignore], 
-        inputCols=[x for x in vectors if x not in ignore], 
+        inputCols=scaled_features, 
         outputCol='features')
 
     train, test = df.randomSplit([0.8, 0.2])
 
     # Pipeline
     pipeline = Pipeline(stages=[
-        # # Transformers
-        # filename_2vec,
-        # artist_id_2vec,
-        # similar_artists_2vec,
+        # Transformers
+        artist_id_tf,
+        artist_name_tf,
+        title_tf,
+        similar_artists_tf,
+        artist_familiarity_tf,
+        artist_hotttnesss_tf,
+        danceability_tf,
+        energy_tf,
+        end_of_fade_in_tf,
+        start_of_fade_out_tf,
+        key_tf,
+        key_confidence_tf,
+        duration_tf,
+        loudness_tf,
+        tempo_tf,
+        mode_tf,
+        time_signature_tf,
+        year_tf,
         # # Assemble everything
-        # final_assembler,
-        VectorAssembler(inputCols=['segments_timbre_vec'], outputCol='features'),
-        KMeans(k=20)    #TODO: assume 20 clusters, optimize later
+        final_assembler,
+        # VectorAssembler(inputCols=['segments_timbre_vec'], outputCol='features'),
+        KMeans(featuresCol='features', k=20)    #TODO: assume 20 clusters, optimize later
     ])
 
     model = pipeline.fit(train)
